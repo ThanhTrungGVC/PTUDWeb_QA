88d44ff57532ca67fd9d371f626ca0ea554d0c79
Nguyễn Thành Trung <44042704+ThanhTrungGVC@users.noreply.github.com>
2019-10-31 20:02:06 +0700

    check login

--- QAweb/check_login.php
+++ QAweb/check_login.php
@@ -7,35 +7,64 @@
 	* - kiểm tra phân quyền người dùng
 	*/
 
-	#session
+	## import connect.php
+		include "database/connect.php";
 
-	#cookie
+	## session
 
-	// Lấy thông tin người dùng nhập vào
-	$input_email = $_POST["email"];
-	$input_pass = md5($_POST["password"]);
+	## cookie
 
-	// Lấy thông tin từ CSDL
-		# connect to database
-			### TODO
 
-		# Lấy kết quả
-			$sql_email = "admin";
-			$sql_pass  = md5("1234");
+	// get input user post
+		$input_user = $_POST["email"];
+		$input_pass = $_POST["password"];
 
-	# Kiểm tra thông tin tài khoản
-	if($input_email == $sql_email && $input_pass == $sql_pass){ // đúng
-		# lấy phân quyền
-			$position = "giao vien";
-		# kiểm tra phân quyền
-			if ($position == "giao vien"){
-				header('Location: /teacher');	// chuyển tới trang của giáo viên
-			} elseif ($position = "hoc sinh") {
-				header('Location: /student');	// chuyển tới trang của học sinh
-			}
-	}else{	// sai
-		# xử lý: yêu cầu người dùng nhập lại
-		echo "Thông tin tài khoản hoặc mật khẩu không chính xác!";
+	// check input null
+	if (!$input_user || !$input_pass) {
+		# TODO mess NULL
+		header('Location: index.php');
 	}
+
+	// get information from database
+		# connect to database
+		$sql = "SELECT role_id, user_names, user_pass, user_status FROM users WHERE user_names = '$input_user' AND user_pass = '$input_pass'";
+		$result = $conn->query($sql);
+
+		if (!$result) {
+			die("Error connect to database! Pleas try again!");
+		}
+
+		# get result
+		if($result->num_rows > 0) {
+			$row = $result->fetch_assoc();
+			$sql_user = $row['user_names'];
+			$sql_pass = $row['user_pass'];
+			$sql_role = $row['role_id'];
+			$sql_status = $row['user_status'];
+		} else {
+			$sql_user = "NULL";
+			$sql_pass = "";
+			$sql_role = "";
+			$sql_status = "";
+		}
+
+	// check account
+		if($input_user == $sql_user && $input_pass == $sql_pass){ // true
+			# check status
+			if($sql_status == "action"){	// true
+				# check role
+				if ($sql_role <= 2){
+					header('Location: teacher');	// go to page teacher
+				} else {
+					header('Location: student');	// go to page student
+				}
+			} else {  // false: account banning
+				# TODO: baning account
+				echo "Account is banning. Try again!";
+			}
+		}else{	// false: incorrect info account
+			# TODO: re-input
+			echo "Thông tin tài khoản hoặc mật khẩu không chính xác!";
+		}
 
 ?>
